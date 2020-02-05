a50f4c592411ddf12ac644fd96cca835008ae577
Stepan Repin <stnrepin@gmail.com>
2020-02-05 21:52:51 +0300

    WIP

---
+++ src/net.h
@@ -0,0 +1,12 @@
+#ifndef NET_H
+#define NET_H
+
+void socket_create();
+void socket_close();
+void socket_bind();
+void socket_prepare();
+void socket_get_client();
+void socket_receive();
+void socket_send();
+
+#endif // !NET_H
