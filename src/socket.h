28ccbc66a9f267a31ddaf89a747690b808e8fb92
Stepan Repin <stnrepin@gmail.com>
2020-02-11 23:44:23 +0300

    Fix bugs: received string doesn't contain '\0' and response has zero byte

--- src/socket.h
+++ src/socket.h
@@ -12,7 +12,8 @@ error_t socket_open(socket_t *sock);
 error_t socket_close(socket_t sock);
 error_t socket_init_server(socket_t sock, uint16_t port, const char *host);
 error_t socket_get_client(socket_t serv_sock, socket_t *client_sock);
-error_t socket_receive_data(socket_t sock, char *buff, size_t buff_size);
+error_t socket_receive_data(socket_t sock, char *buff, size_t buff_size,
+                            size_t *buff_act_size);
 error_t socket_send_data(socket_t sock, const char *buff, size_t buff_size);
 
 #endif // !NET_H
